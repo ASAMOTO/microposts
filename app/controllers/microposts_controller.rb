8f2a13faf5c05b9b8c4cb8d987f2d702688d483b
AsamotoYuki <asamoto@basicinc.jp>
2016-05-23 04:13:37 +0000

    add home view's timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropostを作成しました！"
             redirect_to root_url
         else
+            @feed_items = current_user.feed_items.includes(:user).orader(created_at: :desc)
             render 'static_pages/home'
         end
     end
