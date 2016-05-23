85e62202a12791d531eefe8e1dacbabedd6e8233
AsamotoYuki <asamoto@basicinc.jp>
2016-05-23 04:02:03 +0000

    add following/followed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
+  end
+  
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
   end
   
   def new
